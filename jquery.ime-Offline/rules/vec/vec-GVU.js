bf68b28439037de30ebee1391e90ad9536e6323e
MANOJ KUMAR SAHUKAR <manoj.sahukar@live.com>
2018-05-29 11:35:13 +0530

    Updated all components.
    Help file. Input methods.

--- jquery.ime-Offline/rules/vec/vec-GVU.js
+++ jquery.ime-Offline/rules/vec/vec-GVU.js
@@ -3,7 +3,7 @@
 
 	var vec = {
 		id: 'vec-GVU',
-		name: 'Venetian GVU',
+		name: 'Vèneto GVU',
 		description: 'Venetian input method.',
 		date: '2013-08-06',
 		URL: 'http://github.com/wikimedia/jquery.ime',
@@ -11,20 +11,20 @@
 		license: 'GPLv3',
 		version: '1.0',
 		patterns: [
-			['dh', 'đ'],
-			['lh', 'ƚ'],
-			['òò', 'ó'],
-			['zh', 'ẑ'],
-			['đh', 'dh'],
-			['ƚh', 'lh'],
-			['ẑh', 'zh'],
-			['Dh', 'Đ'],
-			['Lh', 'Ƚ'],
-			['Òò', 'Ó'],
-			['Zh', 'Ẑ'],
-			['Đh', 'Dh'],
-			['Ƚh', 'Lh'],
-			['Ẑh', 'Zh']
+			[ 'dh', 'đ' ],
+			[ 'lh', 'ƚ' ],
+			[ 'òò', 'ó' ],
+			[ 'zh', 'ẑ' ],
+			[ 'đh', 'dh' ],
+			[ 'ƚh', 'lh' ],
+			[ 'ẑh', 'zh' ],
+			[ 'Dh', 'Đ' ],
+			[ 'Lh', 'Ƚ' ],
+			[ 'Òò', 'Ó' ],
+			[ 'Zh', 'Ẑ' ],
+			[ 'Đh', 'Dh' ],
+			[ 'Ƚh', 'Lh' ],
+			[ 'Ẑh', 'Zh' ]
 		]
 	};
 
